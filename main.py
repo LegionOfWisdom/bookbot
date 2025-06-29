8b00eefdd7cdb32c1c97a1cdd946466debd35896
TheeAuthor <mbarri0@outlook.com>
2025-06-29 16:51:34 -0400

    Final

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-def get_book_text(filepath):
-    file_content = ''
-    
-    with open(filepath) as file:
-        file_content = file.read()
-    return file_content
+from stats import get_num_words, char_counter
 
 def main():
-    from stats import word_counter
-    
-    book_path = "/home/mbarrio/workspace/github.com/LegionOfWisdom/bookbot/books/frankenstein.txt"
-    num_words = 0
-    
-    content = get_book_text(book_path)
-    num_words = word_counter(content)
-    
-    print (f"{num_words} words found in the document")
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+    print(char_counter(text))
 
-main ()
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+main()
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
-def word_counter(string):
-    ttl_words = len(string.split())
-    return ttl_words
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def char_counter(text):
+    char_dic = {}
+    
+    for char in text:
+        low_char = char.lower()
+        if low_char not in char_dic:
+            char_dic[low_char] = 1
+        else:
+            char_dic[low_char] +=1
+    return (char_dic)
