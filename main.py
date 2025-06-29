bde8e4c161b8acce2f9248e4766e9cfb4f7055cf
TheeAuthor <mbarri0@outlook.com>
2025-06-29 11:06:38 -0400

    Refacted code into stats.py

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ def get_book_text(filepath):
     return file_content
 
 def main():
+    from stats import word_counter
+    
     book_path = "/home/mbarrio/workspace/github.com/LegionOfWisdom/bookbot/books/frankenstein.txt"
     num_words = 0
     
     content = get_book_text(book_path)
-    ttl_words = len(content.split())
+    num_words = word_counter(content)
     
-    print (f"{ttl_words} words found in the document")
+    print (f"{num_words} words found in the document")
 
 main ()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def word_counter(string):
+    ttl_words = len(string.split())
+    return ttl_words
