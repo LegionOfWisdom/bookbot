18b712f9c7c7d1712587e0f338b7c2683e4e41a2
TheeAuthor <mbarri0@outlook.com>
2025-07-02 17:18:31 -0400

    create ordered dictionary

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-from stats import get_num_words, char_counter
+from stats import ( 
+    get_num_words, 
+    char_counter, 
+    to_be_sorted
+)
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
-    print(char_counter(text))
-
+    chars_dict = char_counter(text)
+    chars_sorted_list = to_be_sorted(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
+    
+    
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")    
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,14 @@ def char_counter(text):
         else:
             char_dic[low_char] +=1
     return (char_dic)
+
+def sort_on(d):
+    return d["num"]
+
+def to_be_sorted(num_char):
+    sorted_list = []
+    for ch in num_char:
+        sorted_list.append({"char":ch, "num":num_char[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)    
+    return sorted_list
+
